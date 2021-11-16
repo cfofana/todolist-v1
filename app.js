45b27c2ff2b3aba1e17c707cb6bcf076028a3811
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2021-11-15 21:28:25 -0500

    transfer data using ejs templating

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs');
  
-app.get('/', (req, res)=>{
-    res.render('index', {foo: 'Foo'});
-})
 
 app.set('view engine', ejs);
 
-
+app.get('/', (req, res)=>{
+    var date = new Date();
+    var currentDay = date.getDay();
+    var day = "";
+    if(currentDay === 6 || currentDay === 0) {
+        day = "Weekend";
+    } else {
+        day = "Weekday";
+    }
+    console.log(day);
+    res.render("index", {kindOfDay: day});
+})
 
 app.listen(3000, ()=>{
     console.log("Server is up and running.");
