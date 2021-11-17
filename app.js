f1ca8f4cc7438f2c959f05cba7664064151ee490
Ceesay Fofana <cfofana51@muleriders.saumag.edu>
2021-11-16 22:13:48 -0500

    display day instead

--- app.js
+++ app.js
@@ -9,12 +9,29 @@ app.get('/', (req, res)=>{
     var date = new Date();
     var currentDay = date.getDay();
     var day = "";
-    if(currentDay === 6 || currentDay === 0) {
-        day = "Weekend";
-    } else {
-        day = "Weekday";
-    }
-    console.log(day);
+    switch(currentDay) {
+        case 0:
+            day="Sunday";
+            break;
+        case 1:
+            day="Monday";
+             break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break; 
+        case 6:
+            day="Saturday";
+            break;
+        }
     res.render("index", {kindOfDay: day});
 })
 
